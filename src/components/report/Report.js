15dad19e80d130de121c949ecbdf68664edf6832
Ido Elad <ido@autoleadstar.com>
2019-08-04 00:16:56 +0300

    fixed all reporting types

--- src/components/report/Report.js
+++ src/components/report/Report.js
@@ -89,14 +89,14 @@ class Report extends Component {
     }
   };
 
-  CategoriesDetails = [
+  MosadCategoriesDetails = [
     {
       key: "physicalConditions",
       CategoryName: "תנאי מחייה",
       questionsAndAnswers: [
         {
-          label: "כמה דיירים חיים בחדר?",
-          options: [1, 2, 3, "יותר"],
+          label: "כמה דיירים גרים בחדר?",
+          options: ["1", "2", "3", "יותר"],
           answer: "",
           type: "radio"
         },
@@ -214,7 +214,7 @@ class Report extends Component {
           label: "אם כן, באילו שירותים?",
           options: ['מתנ"ס', "קופת חולים", "מכולת", "אחר"],
           answer: "",
-          type: "radio"
+          type: "checkbox"
         },
         {
           label: "האם לדיירים יש קשר עם התושבים בקהילה?",
@@ -418,6 +418,608 @@ class Report extends Component {
       ]
     }
   ];
+  HostelCategoriesDetails = [
+    {
+      key: "physicalConditions",
+      CategoryName: "תנאי מחייה",
+      questionsAndAnswers: [
+        {
+          label: "כמה דיירים גרים בחדר?",
+          options: ["1", "2", "3", "יותר"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם האוכל שמוגש בהוסטל מבושל במקום?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם הריהוט שלם ותקין?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "כמה דיירים חולקים מקלחת ושירותים?",
+          options: ["1-2", "3-5", "6 ומעלה"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "כמה דיירים חולקים טלויזיה?",
+          options: ["1-2", "3-5", "6 ומעלה"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "כמה דיירים חולקים מחשב?",
+          options: ["1-2", "3-5", "6 ומעלה"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "OccupationAndDailyRoutine",
+      CategoryName: "תעסוקה וסדר יום",
+      questionsAndAnswers: [
+        {
+          label: "האם הדיירים חייבים לצאת לעבוד כתנאי למגורים בהוסטל?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם דיירים רשאים להישאר בהוסטל בשעות הבוקר (אם אין להם עבודה)?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם המעסיק של הדיירים שייך לאותו גוף שמפעיל את ההוסטל?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "עד כמה לדיירים יש השפעה על סדר היום והפעילות?",
+          options: ["השפעה רבה", "השפעה בינונית", "השפעה נמוכה", "אין השפעה בכלל"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם הדיירים יכולים להירשם לחוגים מחוץ להוסטל?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "כמה פעמים בשבוע מתקיימת פעילות תרבות ופנאי בהוסטל",
+          options: ["פעם אחת", "פעמיים", "שלוש פעמים", "יותר"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם הדיירים יוצאים לבילוי מחוץ להוסטל?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "אם כן, לאיזה סוג של פעילות?",
+          answer: "",
+          type: "input"
+        }
+      ]
+    },
+    {
+      key: "CommunicationWithFamily",
+      CategoryName: "קשר עם המשפחה",
+      questionsAndAnswers: [
+        {
+          label: "האם אפשר לבקר את הדיירים בכל זמן?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם בני משפחה יכולים להיכנס לחדר?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "באיזו תדירות הדיירים יוצאים הביתה?",
+          options: ["כל שבוע", "פעם בשבועיים", "פעם בחודש", "אחר"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "CommunityRelations",
+      CategoryName: "קשר עם הקהילה",
+      questionsAndAnswers: [
+        {
+          label: "האם הדיירים משתמשים בשירותים הקהילתיים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "אם כן, באילו שירותים?",
+          options: ['מתנ"ס', "קופת חולים", "מכולת", "אחר"],
+          answer: "",
+          type: "checkbox"
+        },
+        {
+          label: "האם לדיירים יש קשר עם התושבים בקהילה?",
+          options: ["בכלל לא", "מעט", "הרבה"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "SleepingHoures",
+      CategoryName: "שעות שינה",
+      questionsAndAnswers: [
+        {
+          label: "מתי הולכים לישון בהוסטל?",
+          options: ["8", "9", "10", "אין שעה קבועה"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם יש כיבוי אורות?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם מחייבים את הדיירים לנוח אחר הצהריים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "Independence",
+      CategoryName: "עצמאות",
+      questionsAndAnswers: [
+        {
+          label: "האם המטבח בהוסטל פתוח עבור הדיירים כל הזמן?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם דיירים חשופים לדיווחים שנכתבים עליהם?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם הדיירים משתתפים בקביעת הפעילות וסדר היום?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "Crew",
+      CategoryName: "התנהלות הצוות",
+      questionsAndAnswers: [
+        {
+          label: "מה היחס של הצוות לדיירים?",
+          options: ["מצוין", "טוב", "סביר", "לא טוב", "רע"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם אנשי הצוות משתמשים באלימות מילולית?",
+          options: ["בכלל לא", "מעט", "הרבה"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם קיימת אלימות פיזית במוסד?",
+          options: ["בכלל לא", "מעט", "הרבה"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם אנשי הצוות מענישים דיירים?",
+          options: ["בכלל לא", "מעט", "הרבה"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "אם כן, מהי צורת הענישה המקובלת במוסד?",
+          type: "input",
+          answer: ""
+        },
+        {
+          label: "האם הדיירים זוכים ליחס נעים ולהקשבה?",
+          options: ["בכלל לא", "מעט", "הרבה"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "Movement",
+      CategoryName: "הגבלת תנועה",
+      questionsAndAnswers: [
+        {
+          label: "האם ההוסטל נעול במהלך היום?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם דייר צריך לקבל אישור כדי לצאת מההוסטל?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם דיירים מקבלים אישור לצאת לחופשות בבית בקלות?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם משתמשים באמצעי הגבלה וכבילה (כסאות הגבלה, קשירה למיטה, נעילה בחדר)?",
+          options: ["בכלל לא", "מעט", "הרבה"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "Misc",
+      CategoryName: "שונות",
+      questionsAndAnswers: [
+        {
+          label: "האם הדיירים או בני משפחתם נדרשים לתוספת תשלום להוסטל?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם לקיחת תרופות היא תנאי למגורים בהוסטל?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם לדעתך נותנים לדיירים תרופות שלא לצורך?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+      ]
+    },
+    {
+      key: "Inspection",
+      CategoryName: "פיקוח",
+      questionsAndAnswers: [
+        {
+          label: "האם כבר הגשת בעבר תלונות על המוסד?",
+          options: ["לא", "כן"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "אם כן,תוך כמה זמן התלונות או הבקשות מטופלות?",
+          options: ["יום", "כמה ימים", "שבוע", "מספר שבועות", "לא מטופלות"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם יש לך קשר ישיר עם הגורם המפקח על המוסד?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם הוא זמין בעת הצורך?",
+          options: ["לא", "כן"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "GeneralNotes",
+      CategoryName: "התרשמות כללית והערות",
+      questionsAndAnswers: [
+        {
+          answer: "",
+          type: "text"
+        }
+      ]
+    }
+  ];
+  PsycCategoriesDetails = [
+    {
+      key: "physicalConditions",
+      CategoryName: "תנאי מחייה",
+      questionsAndAnswers: [
+        {
+          label: "כמה דיירים גרים בחדר?",
+          options: ["1", "2", "3", "יותר"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "אילו פריטים קיימים במחלקה?",
+          options: [
+            "ספרים",
+            "מחשב",
+            "טלוויזיה",
+            "משחקים",
+            "חומרי יצירה",
+            "ספות ישיבה",
+            "נגן מוזיקה"
+          ],
+          answer: "",
+          type: "checkbox"
+        },
+        {
+          label: "האם הריהוט שלם ותקין?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "כמה דיירים חולקים מקלחת ושירותים?",
+          options: ["1-2", "3-5", "6 ומעלה"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "PersonalBelongings",
+      CategoryName: "חפצים אישיים",
+      questionsAndAnswers: [
+        {
+          label: "האם לאנשים באשפוז יש בגדים אישיים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם מכריחים את האנשים באשפוז ללבוש לבוש של בית החולים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם יש ארון אישי לכל אחד?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם יש לאנשים באשפוז מוצרי היגיינה אישיים (כמו שמפו או מברשת שיניים)?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם יש לאנשים באשפוז מגבת אישית?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+
+      ]
+    },
+    {
+      key: "OccupationAndDailyRoutine",
+      CategoryName: "טיפול וסדר יום",
+      questionsAndAnswers: [
+        {
+          label: "כמה זמן אדם ממתין מרגע תחילת האשפוז ועד למפגש הראשון שלו עם פסיכיאטר מטפל?",
+          options: ["עד יום", "בין יום 3 ימים", "בין 3 ימים ל-5 ימים", "יותר מ-5 ימים"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "כמה פעמים בשבוע אדם נפגש עם פסיכולוג או פסיכיאטר?",
+          options: ["1", "2", "3", "מעל 3"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם גורמי הטיפול משתפים את האדם המאושפז בתהליך קבלת ההחלטות על הטיפול התרופתי?",
+          options: ["כן", "לא", "לא יודע"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "מרגע הדיווח על מצוקה נפשית במהלך האשפוז, כמה זמן עובר עד שגורם טיפולי (פסיכולוג, עו\"ס או פסיכיאטר)נותן מענה למצוקה?",
+          options: ["באופן מיידי", "במהלך היום", "בין יום ל-3 ימים", "מעל 3 ימים"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "במהלך היום, האם יש תעסוקה ופנאי עבור האנשים המאושפזים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+      ]
+    },
+    {
+      key: "CommunicationWithFamily",
+      CategoryName: "קשר עם המשפחה",
+      questionsAndAnswers: [
+        {
+          label: "האם אפשר לבקר את הדיירים בכל זמן?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם בני משפחה יכולים להיכנס לחדר?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "באיזו תדירות הדיירים יוצאים הביתה?",
+          options: ["כל שבוע", "פעם בשבועיים", "פעם בחודש", "אחר"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "CommunityRelations",
+      CategoryName: "קשר עם אנשים מהחוץ",
+      questionsAndAnswers: [
+        {
+          label: "האם האנשים רשאים להשתמש בטלפון הנייד שבבעלותם?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם אפשר לקבל מבקרים בכל יום?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם בני משפחה יכולים להיכנס למחלקה או לחדר?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם בית החולים שיתף קרוב משפחה של האדם במצבו?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        }
+      ]
+    },
+    {
+      key: "Kfiya",
+      CategoryName: "כפייה באשפוז",
+      questionsAndAnswers: [
+        {
+          label: "האם אנשי הצוות מאיימים באישפוז כפוי על אנשים שנמצאים באשפוז בהסכמה?",
+          options: ["כן", "לא", "לא יודע"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם אנשי הצוות משתפים אנשים שנמצאים באשפוז כפוי בזכות שלהם לערער?",
+          options: ["כן", "לא", "לא יודע"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם אנשים שנמצאים באשפוז בכפייה, רשאים לקבל סיוע משפטי ולדבר עם עורך דין לפני יום הדיון?",
+          options: ["כן", "לא", "לא יודע"],
+          answer: "",
+          type: "radio"
+        },
+      ]
+    },
+    {
+      key: "Violence",
+      CategoryName: "אירועי אלימות",
+      questionsAndAnswers: [
+        {
+          label: "איך הייתם מגדירים את היחס של הצוות לאנשים המאושפזים?",
+          options: ["מצוין", "טוב", "סביר", "לא טוב", "רע"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "כמה אתם מרגישים בטוחים במחלקה?",
+          options: ["מאוד בטוחים", "קצת בטוחים", "בכלל לא בטוחים"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם חוויתם אלימות מצד מטופל אחר?",
+          options: ["אלימות מילולית", "השפלה", "אלימות פיזית"],
+          answer: "",
+          type: "checkbox"
+        },
+        {
+          label: "האם חוויתם אלימות מצד איש צוות?",
+          options: ["אלימות מילולית", "השפלה", "אלימות פיזית"],
+          answer: "",
+          type: "checkbox"
+        },
+        {
+          label: "האם ראיתם אלימות של איש צוות כלפי מטופלים אחרים?",
+          options: ["אלימות מילולית", "השפלה", "אלימות פיזית"],
+          answer: "",
+          type: "checkbox"
+        },
+        {
+          label: "האם חוויתם או ראיתם קשירה של מטופלים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם חוויתם או ראיתם בידוד של מטופלים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם חוויתם או ראיתם קשירה כימית או זריקות שנועדו לניטרול מטופלים?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם התלוננת במחלקה?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "האם ציינו בפניכם שזכותכם להגיש תלונה למנהל בית החולים או למשרד הבריאות?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+        {
+          label: "במקרה והגשתם תלונה, האם הטיפול בה היה יעיל לדעתכם?",
+          options: ["כן", "לא"],
+          answer: "",
+          type: "radio"
+        },
+
+      ]
+    },
+    {
+      key: "GeneralNotes",
+      CategoryName: "התרשמות כללית והערות",
+      questionsAndAnswers: [
+        {
+          answer: "",
+          type: "text"
+        }
+      ]
+    }
+  ];
 
   goToStep = step => {
     this.setState({
@@ -541,9 +1143,23 @@ class Report extends Component {
         break;
       case "reportSubjects":
         this.nextStep = this.step4;
+        let categoryDetails;
+        switch (this.state.values.instituteType) {
+          case 'מוסד':
+            categoryDetails = this.MosadCategoriesDetails;
+            break;
+          case 'בית חולים פסיכיאטרי':
+            categoryDetails = this.PsycCategoriesDetails;
+            break;
+          case 'הוסטל':
+            categoryDetails = this.HostelCategoriesDetails;
+            break;
+          default:
+            categoryDetails = this.MosadCategoriesDetails;
+        }
         this.formPart = (
           <ReportSubjects
-            CategoriesDetails={this.CategoriesDetails}
+            CategoriesDetails={categoryDetails}
             handleChange={this.handleChange}
             handleClick={this.handleClick}
             handleSelect={this.handleSelect}
